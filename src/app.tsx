afd71cb3b120b63b7c46dfe0fdb6c34fc4ed383f
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-02-14 10:42:07 -0300

    update toggle All Tasks and styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 export default function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch(
+        "https://my-json-server.typicode.com/EnkiGroup/DesafioReactFrontendJunior2024/todos"
+      );
+
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.error("Erro ao recuperar as tarefas:", error);
+    }
+  };
+
+  const addTask = () => {
+    if (newTaskTitle === "") return;
+
+    const newTask: Task = {
+      id: Date.now(),
+      title: newTaskTitle,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle("");
+  };
+
+  const removeTask = (taskId: number) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const toggleTaskCompletion = (taskId: number) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const toggleAllTasksCompletion = () => {
+    const allCompleted = tasks.every((task) => task.completed);
+    const updatedTasks = tasks.map((task) => ({
+      ...task,
+      completed: !allCompleted,
+    }));
+    setTasks(updatedTasks);
+  };
+
   return (
     <section className={styles.containerSection}>
       <h1>todos</h1>
 
       <div className={styles.containerSearch}>
         <div className={styles.containerCheckbox}>
-          <input className={styles.checkbox} type="checkbox" name="checkbox" />
-          <label htmlFor="checkbox">Toggle All Input</label>
+          <input
+            className={styles.checkbox}
+            type="checkbox"
+            name="checkbox"
+            onClick={toggleAllTasksCompletion}
+          />
+          <label></label>
         </div>
 
         <div className={styles.containerInput}>
@@ -17,31 +82,38 @@ export default function App() {
             className={styles.input}
             type="text"
             placeholder="What needs to be done?"
+            value={newTaskTitle}
+            onChange={(event) => setNewTaskTitle(event.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && addTask()}
           />
         </div>
       </div>
 
       <main className={styles.main}>
         <ul className={styles.todoList}>
-          <li>
-            <div className={styles.view}>
-              <input className={styles.toggle} type="checkbox" />
-              <label>123</label>
-              <button className={styles.destroy}></button>
-            </div>
-          </li>
-          <li>
-            <div className={styles.view}>
-              <input className={styles.toggle} type="checkbox" />
-              <label>45683</label>
-              <button className={styles.destroy} />
-            </div>
-          </li>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <div className={styles.view}>
+                <input
+                  className={styles.toggle}
+                  type="checkbox"
+                  onClick={() => toggleTaskCompletion(task.id)}
+                  checked={task.completed}
+                />
+
+                <label>{task.title}</label>
+                <button
+                  className={styles.destroy}
+                  onClick={() => removeTask(task.id)}
+                ></button>
+              </div>
+            </li>
+          ))}
         </ul>
       </main>
 
       <footer className={styles.footer}>
-        <span className={styles.todoCount}>2 items left!</span>
+        <span className={styles.todoCount}>{tasks.length} items left!</span>
         <ul className={styles.filters}>
           <li>
             <a href="#">All</a>
@@ -50,7 +122,7 @@ export default function App() {
             <a href="#">Active</a>
           </li>
           <li>
-            <a href="#">Active</a>
+            <a href="#">Completed</a>
           </li>
         </ul>
         <button className={styles.clearCompleted}>Clear completed</button>
