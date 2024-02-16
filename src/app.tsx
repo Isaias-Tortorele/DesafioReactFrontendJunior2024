e634bae6432a0df5f1b34b744db9e2ff1fe5762a
Isaias Tortorele <isaiastortorele23@gmail.com>
2024-02-15 22:44:06 -0300

    update

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import styles from "./styles.module.css";
 
 interface Task {
@@ -12,6 +14,7 @@ export default function App() {
   const [newTaskTitle, setNewTaskTitle] = useState("");
   const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
   const [editingTaskTitle, setEditingTaskTitle] = useState("");
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     fetchTasks();
@@ -57,11 +60,16 @@ export default function App() {
 
   const toggleAllTasksCompletion = () => {
     const allisDone = tasks.every((task) => task.isDone);
+
     const updatedTasks = tasks.map((task) => ({
       ...task,
       isDone: !allisDone,
     }));
     setTasks(updatedTasks);
+  };
+
+  const countTasks = () => {
+    return tasks.filter((task) => !task.isDone).length;
   };
 
   const handleDoubleClick = (taskId: number, title: string) => {
@@ -88,94 +96,139 @@ export default function App() {
   };
 
   const clearCompleted = () => {
-    setTasks([]);
+    setTasks(tasks.filter((task) => !task.isDone));
+  };
+
+  const changeFilter = (selectedFilter: string) => {
+    setFilter(selectedFilter);
   };
 
   return (
-    <section className={styles.containerSection}>
-      <h1>todos</h1>
+    <Router>
+      <section className={styles.containerSection}>
+        <h1>todos</h1>
 
-      <div className={styles.containerSearch}>
-        <div className={styles.containerCheckbox}>
-          <input
-            className={styles.checkbox}
-            type="checkbox"
-            onClick={toggleAllTasksCompletion}
-          />
-          <label></label>
+        <div className={styles.containerSearch}>
+          <div className={styles.containerCheckbox}>
+            <input
+              className={styles.checkbox}
+              type="checkbox"
+              onClick={toggleAllTasksCompletion}
+            />
+            {tasks.length !== 0 && (
+              <label onClick={toggleAllTasksCompletion}></label>
+            )}
+          </div>
+
+          <div className={styles.containerInput}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="What needs to be done?"
+              value={newTaskTitle}
+              onChange={(event) => setNewTaskTitle(event.target.value)}
+              onKeyDown={(element) => element.key === "Enter" && addTask()}
+            />
+          </div>
         </div>
 
-        <div className={styles.containerInput}>
-          <input
-            className={styles.input}
-            type="text"
-            placeholder="What needs to be done?"
-            value={newTaskTitle}
-            onChange={(event) => setNewTaskTitle(event.target.value)}
-            onKeyDown={(element) => element.key === "Enter" && addTask()}
-          />
-        </div>
-      </div>
+        <main className={styles.main}>
+          <ul className={styles.todoList}>
+            {tasks
+              .filter((task) => {
+                if (filter === "active") {
+                  return !task.isDone;
+                } else if (filter === "completed") {
+                  return task.isDone;
+                } else {
+                  return task;
+                }
+              })
+              .map((task) => (
+                <li
+                  key={task.id}
+                  className={task.isDone ? styles.completed : ""}
+                >
+                  <div className={styles.view}>
+                    <input
+                      className={styles.toggle}
+                      type="checkbox"
+                      onChange={() => toggleTaskCompletion(task.id)}
+                      checked={task.isDone}
+                    />
 
-      <main className={styles.main}>
-        <ul className={styles.todoList}>
-          {tasks.map((task) => (
-            <li key={task.id} className={task.isDone ? styles.completed : ""}>
-              <div className={styles.view}>
-                <input
-                  className={styles.toggle}
-                  type="checkbox"
-                  onClick={() => toggleTaskCompletion(task.id)}
-                  checked={task.isDone}
-                />
+                    {editingTaskId === task.id ? (
+                      <input
+                        className={styles.edit}
+                        type="text"
+                        value={editingTaskTitle}
+                        onChange={handleEditChange}
+                        onBlur={() => setEditingTaskId(null)}
+                        onKeyDown={(element) =>
+                          handleEditKeyDown(element, task.id)
+                        }
+                      />
+                    ) : (
+                      <label
+                        onDoubleClick={() =>
+                          handleDoubleClick(task.id, task.title)
+                        }
+                      >
+                        {task.title}
+                      </label>
+                    )}
 
-                {editingTaskId === task.id ? (
-                  <input
-                    className={styles.edit}
-                    type="text"
-                    value={editingTaskTitle}
-                    onChange={handleEditChange}
-                    onBlur={() => setEditingTaskId(null)}
-                    onKeyDown={(element) => handleEditKeyDown(element, task.id)}
-                  />
-                ) : (
-                  <label
-                    onDoubleClick={() => handleDoubleClick(task.id, task.title)}
-                  >
-                    {task.title}
-                  </label>
-                )}
+                    <button
+                      className={styles.destroy}
+                      onClick={() => removeTask(task.id)}
+                    ></button>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </main>
 
-                <button
-                  className={styles.destroy}
-                  onClick={() => removeTask(task.id)}
-                ></button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </main>
-
-      <footer className={styles.footer}>
-        <span className={styles.todoCount}>{tasks.length} items left!</span>
-        <ul className={styles.filters}>
-          <li>
-            <a href="#">All</a>
-          </li>
-          <li>
-            <a href="#">Active</a>
-          </li>
-          <li>
-            <a href="#">Completed</a>
-          </li>
-        </ul>
-        <button
-          className={styles.clearCompleted}
-          onClick={() => clearCompleted()}
-        >
-          Clear completed
-        </button>
-      </footer>
-    </section>
+        {tasks.length !== 0 && (
+          <footer className={styles.footer}>
+            <span className={styles.todoCount}>{countTasks()} items left!</span>
+            <ul className={styles.filters}>
+              <li>
+                <Link
+                  to="/"
+                  className={filter === "all" ? styles.selected : ""}
+                  onClick={() => changeFilter("all")}
+                >
+                  All
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/active"
+                  className={filter === "active" ? styles.selected : ""}
+                  onClick={() => changeFilter("active")}
+                >
+                  Active
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/completed"
+                  className={filter === "completed" ? styles.selected : ""}
+                  onClick={() => changeFilter("completed")}
+                >
+                  Completed
+                </Link>
+              </li>
+            </ul>
+            <button
+              className={styles.clearCompleted}
+              onClick={() => clearCompleted()}
+            >
+              Clear completed
+            </button>
+          </footer>
+        )}
+      </section>
+    </Router>
   );
 }
